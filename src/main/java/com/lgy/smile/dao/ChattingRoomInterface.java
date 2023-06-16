f887a6f56e8cae976e93fe3bdc6d86bd4e84b74e
hjm8333 <35636560+hjm8333@users.noreply.github.com>
2023-06-16 14:52:26 +0900

    ChattingRoom con,ser,dto,dao,mapper 추가
    mf파일 ignore, noticedao 경로수정

---
+++ src/main/java/com/lgy/smile/dao/ChattingRoomInterface.java
@@ -0,0 +1,14 @@
+package com.lgy.smile.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.lgy.smile.dto.ChattingRoomDto;
+
+public interface ChattingRoomInterface {
+	public ArrayList<ChattingRoomDto> list();
+	public ChattingRoomDto contentView();
+	public void write(@RequestParam HashMap<String, String> params);
+}
---
+++ src/main/java/com/lgy/smile/dto/ChattingRoomDto.java
@@ -0,0 +1,15 @@
+package com.lgy.smile.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChattingRoomDto {
+	private int identity;
+	private int board;
+	private int seller;
+	private int buyer;
+}
